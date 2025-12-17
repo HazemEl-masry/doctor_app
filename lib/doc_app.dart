bd4d6862e1466142b068bbaed84feff01e57a316
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2025-12-17 02:00:09 +0200

    Add ColorsManager for theme colors and update DocApp with new design size and theme

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -1,20 +1,25 @@
 import 'package:doctor_app/core/router/app_router.dart';
+import 'package:doctor_app/core/theme/colors_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
   const DocApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp.router(
+          title: "DocDoc",
           debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: ColorsManager.mainBlue,
+            scaffoldBackgroundColor: Colors.white
+          ),
           routerConfig: appRouter,
         );
       },
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:doctor_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(DocApp());
+  runApp(const DocApp());
 }
